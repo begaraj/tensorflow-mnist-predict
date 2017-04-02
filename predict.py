aa89f6ac607f716842e468da9e0c2aed3f6b0cdd
Josh Chen <jwcstar@gmail.com>
2017-04-03 00:23:59 +0200

    Some minor changes to code
    - Print test accuracy every 100 steps.  Print both train/test accuracy
      at end
    - Print train/test accuracy based on random sample of entire train/test
      set.
    - Give names to important tensors in create_model.py.  Use these names
      when feeding `sess.run` and obtaining tensors in predict.py
    - Do not construct new graph in predict.py - we already load the saved
      graph via the .meta file.
    - Put code in functions in create_model.py.  Easier to test

--- predict.py
+++ predict.py
@@ -29,61 +29,8 @@ import sys
 import tensorflow as tf
 from PIL import Image, ImageFilter
 
+
 def predictint(imvalue):
-    """
-    This function returns the predicted integer.
-    The imput is the pixel values from the imageprepare() function.
-    """
-    
-    # Define the model (same as when creating the model file)
-    x = tf.placeholder(tf.float32, [None, 784])
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros([10]))
-    
-    def weight_variable(shape):
-      initial = tf.truncated_normal(shape, stddev=0.1)
-      return tf.Variable(initial)
-    
-    def bias_variable(shape):
-      initial = tf.constant(0.1, shape=shape)
-      return tf.Variable(initial)
-       
-    def conv2d(x, W):
-      return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-    
-    def max_pool_2x2(x):
-      return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')   
-    
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
-    
-    x_image = tf.reshape(x, [-1,28,28,1])
-    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
-    
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-    
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-    
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
-    b_fc1 = bias_variable([1024])
-    
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    
-    keep_prob = tf.placeholder(tf.float32)
-    h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    
-    W_fc2 = weight_variable([1024, 10])
-    b_fc2 = bias_variable([10])
-    
-    y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
-    
-    init_op = tf.initialize_all_variables()
-    saver = tf.train.Saver()
-    
     """
     Load the model2.ckpt file
     file is stored in the same directory as this python script is started
@@ -93,6 +40,7 @@ def predictint(imvalue):
     https://www.tensorflow.org/versions/master/how_tos/variables/index.html
     """
     with tf.Session() as sess:
+        init_op = tf.initialize_all_variables()
         sess.run(init_op)
 
         #saver.restore(sess, "model2.ckpt")
@@ -102,9 +50,10 @@ def predictint(imvalue):
         new_saver.restore(sess, tf.train.latest_checkpoint('./'))
         
         #print ("Model restored.")
-       
-        prediction=tf.argmax(y_conv,1)
-        return prediction.eval(feed_dict={x: [imvalue],keep_prob: 1.0}, session=sess)
+        prediction = tf.get_default_graph().get_tensor_by_name('prediction:0')
+        return prediction.eval(feed_dict={
+                'x:0': [imvalue],
+                'keep_prob:0': 1.0}, session=sess)
 
 
 def imageprepare(argv):
@@ -136,14 +85,14 @@ def imageprepare(argv):
         wleft = int(round(((28 - nwidth)/2),0)) #caculate vertical pozition
         newImage.paste(img, (wleft, 4)) #paste resized image on white canvas
     
-    #newImage.save("sample.png")
+    newImage.save("sample.png")
 
     tv = list(newImage.getdata()) #get pixel values
     
     #normalize pixels to 0 and 1. 0 is pure white, 1 is pure black.
     tva = [ (255-x)*1.0/255.0 for x in tv] 
     return tva
-    #print(tva)
+
 
 def main(argv):
     """
@@ -153,5 +102,6 @@ def main(argv):
     predint = predictint(imvalue)
     print (predint[0]) #first value in list
     
+
 if __name__ == "__main__":
     main(sys.argv[1])
